203d70efd858b52891afa609748da78db4dd7042
DevExpressExampleBot <fakeEmail>
2021-10-25 11:30:40 +0300

    Source auto update [skip ci]

---
+++ VB/WpfPivot_GettingStarted_Olap/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace WpfPivot_GettingStarted_Olap
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
